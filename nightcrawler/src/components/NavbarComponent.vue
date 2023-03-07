44728cd1c8c59edeff43706978badd66838fb84e
Meezaan Davids <meezaandavids365@gamil.com>
2023-03-07 16:48:15 +0200

    carousel

--- nightcrawler/src/components/NavbarComponent.vue
+++ nightcrawler/src/components/NavbarComponent.vue
@@ -1,7 +1,7 @@
 <template>
-    <nav class="navbar navbar-expand-lg fixed-top bg-danger-subtle">
+    <nav class="navbar navbar-expand-lg bg-danger-subtle" style="background-color: transparent;">
         <div class="container-fluid">
-            <img class="img-fluid" style="height: 5rem;" src="https://i.postimg.cc/65wLN7d3/nav.png" alt="">
+            <img class="img-fluid" style="height: 4rem;" src="https://i.postimg.cc/65wLN7d3/nav.png" alt="">
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
                 aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -50,4 +50,7 @@ export default {
 
 }
 </script>
-<style scoped></style>
+<style scoped>
+
+
+</style>
--- nightcrawler/src/views/LandingView.vue
+++ nightcrawler/src/views/LandingView.vue
@@ -1,11 +1,49 @@
 <template>
   <main>
     <section id="home">
-
+      <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+          <div class="carousel-item active" data-bs-interval="3000">
+            <img src="https://i.postimg.cc/RVRhr2QQ/man.jpg" class="img-fluid" style="width: 100%;height:90vh" alt="...">
+            <div class="carousel-caption d-none d-md-block">
+              <h5>First slide label</h5>
+              <p>Some representative placeholder content for the first slide.</p>
+            </div>
+          </div>
+          <div class="carousel-item" data-bs-interval="3000">
+            <img src="https://i.postimg.cc/nczbz5f1/city.jpg" class="img-fluid" style="width: 100%;height:90vh"
+              alt="...">
+            <div class="carousel-caption d-none d-md-block">
+              <h5>Second slide label</h5>
+              <p>Some representative placeholder content for the second slide.</p>
+            </div>
+          </div>
+          <div class="carousel-item"  data-bs-interval="3000">
+            <img src="https://i.postimg.cc/jjC4nHdm/match.jpg" class="img-fluid" style="width: 100%;height:90vh"
+              alt="...">
+            <div class="carousel-caption d-none d-md-block">
+              <h5>Third slide label</h5>
+              <p>Some representative placeholder content for the third slide.</p>
+            </div>
+          </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval"
+          data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval"
+          data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
     </section>
 
     <section id="about">
-
+      <div class="container">
+        <h2 class="text-center">About</h2>
+      </div>
     </section>
 
     <section id="sample">
@@ -26,3 +64,12 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+
+#about {
+  height: 90vh;
+  background-color: blue;
+}
+
+</style>
