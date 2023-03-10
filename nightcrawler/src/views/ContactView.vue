bab3bf2f7b302f63590bcf1649f95e3372ab4ac4
Meezaan Davids <meezaandavids365@gamil.com>
2023-03-10 09:35:54 +0200

    responsive carousel

--- nightcrawler/src/views/ContactView.vue
+++ nightcrawler/src/views/ContactView.vue
@@ -1,18 +1,204 @@
 <template>
-    <main>
-        <section id="contact">
-         
-        </section>
-    </main>
-
-  </template>
-  
-  <script>
-  
-  
-  export default {
-    components: {
-      
-    }
+  <main>
+      <section id="contact">
+        <form action="https://formspree.io/f/mbjbywlp" method="post">
+          <div class="background bg-dark-subtle">
+            <div class="container">
+              <div class="screen bg-dark-subtle">
+                <div class="screen-body">
+                  <div class="screen-body-item left">
+                    <div class="app-title">
+                      <img class="img-fluid" style="height:15rem; padding:20px;" src="https://i.postimg.cc/D0yFCVCf/transparent.png" alt="">
+                    </div>
+                    <div class="app-contact">
+                      <h6 class="heading-text">Catch us on</h6>
+                      <ul>
+                        <a href="/" target="_blank"><li class="list-items"><i class="fa-brands fa-instagram text-dark"></i></li></a>
+                        <a href="/" target="_blank"><li class="list-items"><i class="fa-brands fa-facebook text-dark"></i></li></a>
+                        <a href="/" target="_blank"><li class="list-items"><i class="fa-brands fa-twitter text-dark"></i></li></a>
+                        <a href="/" target="_blank"><li class="list-items"><i class="fa-brands fa-tiktok text-dark"></i></li></a>
+                      </ul>
+                  </div>
+                  </div>
+                  <div class="screen-body-item">
+                    <div class="app-form">
+                      <div class="app-form-group">
+                        <input type="text" name="first-name" class="app-form-control bg-dark-subtle" required placeholder="FIRST NAME">
+                      </div>
+                      <div class="app-form-group">
+                        <input type="text" name="last-name" class="app-form-control bg-dark-subtle" required placeholder="LAST NAME">
+                      </div>
+                      <div class="app-form-group">
+                        <input type="email" name="email" class="app-form-control bg-dark-subtle" required placeholder="EMAIL ADDRESS">
+                      </div>
+                      <div class="app-form-group">
+                        <input type="number" name="number" class="app-form-control bg-dark-subtle" required placeholder="CONTACT NO.">
+                      </div>
+                      <div class="app-form-group message">
+                        <input type="text" name="message" class="app-form-control bg-dark-subtle" required placeholder="MESSAGE">
+                      </div>
+                      <div class="app-form-group buttons">
+                        <button type="submit" class="app-form-button">SUBMIT</button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+      </form>
+      </section>
+  </main>
+</template>
+<script>
+export default {
+  components: {
   }
-  </script>
+}
+</script>
+<style>
+button, input, .heading-text {
+  font-family: 'Montserrat', sans-serif;
+  font-weight: 700;
+  letter-spacing: 1.4px;
+}
+.heading-text {
+  font-family: 'Special Elite', cursive;
+  color: black;
+  font-weight: 700;
+  text-shadow: 2px 2px 4px rgb(134, 131, 131);
+  text-align: center;
+}
+.background {
+  display: flex;
+  min-height: 100vh;
+}
+.container {
+  flex: 0 1 700px;
+  margin: auto;
+  padding: 10px;
+}
+.screen {
+  position: relative;
+  border-radius: 15px;
+  box-shadow: 0 20px 30px rgba(0,0,0,0.2);
+  border: 1px solid white;
+}
+.screen:after {
+  content: '';
+  display: block;
+  position: absolute;
+  top: 0;
+  left: 20px;
+  right: 20px;
+  bottom: 0;
+  border-radius: 15px;
+  box-shadow: 0 20px 40px rgba(0, 0, 0, .4);
+  z-index: -1;
+}
+ul {
+  list-style: none;
+  display: flex;
+  gap: 2rem;
+  font-size: 15px;
+}
+ul:a {
+  color: black;
+}
+.screen-body {
+  display: flex;
+}
+.screen-body-item {
+  flex: 1;
+  padding: 50px;
+}
+.screen-body-item.left {
+  display: flex;
+  flex-direction: column;
+}
+.app-title {
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  font-size: 26px;
+}
+.app-contact {
+  margin-top: auto;
+  font-size: 8px;
+  color: #888;
+}
+.app-form-group {
+  margin-bottom: 15px;
+}
+.app-form-group.message {
+  margin-top: 40px;
+}
+.app-form-group.buttons {
+  margin-bottom: 0;
+  text-align: right;
+  color: black;
+}
+.app-form-control {
+  width: 100%;
+  padding: 10px 0;
+  background: none;
+  border: none;
+  border-bottom: 1px solid #666;
+  font-family: 'Special Elite', cursive;
+  color: black;
+  font-size: 14px;
+  text-transform: uppercase;
+  outline: none;
+  transition: border-color .2s;
+}
+.app-form-control::placeholder {
+  font-family: 'Special Elite', cursive;
+  color: black;
+  font-weight: 700;
+  text-shadow: 2px 2px 4px rgb(134, 131, 131);
+}
+.app-form-control:focus {
+  border-bottom-color: #ddd;
+}
+.app-form-button {
+  background: none;
+  border: 1px solid white;
+  padding: 5px;
+  color: black;
+  font-size: 14px;
+  cursor: pointer;
+  outline: none;
+  font-family: 'Special Elite', cursive;
+  font-weight: 700;
+  text-shadow: 2px 2px 4px rgb(134, 131, 131);
+}
+.app-form-button:hover {
+  color: beige;
+  background: black;
+}
+@media screen and (max-width: 300px) {
+  .screen-body {
+    flex-direction: column;
+  }
+  .screen-body-item.left {
+    margin-bottom: 30px;
+  }
+  .app-title {
+    flex-direction: row;
+  }
+  .app-title span {
+    margin-right: 12px;
+  }
+  .app-title:after {
+    display: none;
+  }
+}
+@media screen and (max-width: 300px) {
+  .screen-body {
+    padding: 40px;
+  }
+  .screen-body-item {
+    padding: 0;
+  }
+}
+</style>
--- nightcrawler/src/views/LandingView.vue
+++ nightcrawler/src/views/LandingView.vue
@@ -4,14 +4,14 @@
       <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
         <div class="carousel-inner">
           <div class="carousel-item active" data-bs-interval="3000">
-            <img src="https://i.postimg.cc/3RggZmPj/bulb.jpg" class="img-fluid" style="width: 100%;height:90vh" alt="...">
+            <img src="https://i.postimg.cc/3RggZmPj/bulb.jpg" class="img-fluid" style="width: 100%;max-height:90vh" alt="...">
             <div class="carousel-caption d-none d-md-block">
               <h5>First slide label</h5>
               <p>Some representative placeholder content for the first slide.</p>
             </div>
           </div>
           <div class="carousel-item" data-bs-interval="3000">
-            <img src="https://i.postimg.cc/nczbz5f1/city.jpg" class="img-fluid" style="width: 100%;height:90vh"
+            <img src="https://i.postimg.cc/nczbz5f1/city.jpg" class="img-fluid" style="width: 100%;max-height:90vh"
               alt="...">
             <div class="carousel-caption d-none d-md-block">
               <h5>Second slide label</h5>
@@ -19,7 +19,7 @@
             </div>
           </div>
           <div class="carousel-item"  data-bs-interval="3000">
-            <img src="https://i.postimg.cc/jjC4nHdm/match.jpg" class="img-fluid" style="width: 100%;height:90vh"
+            <img src="https://i.postimg.cc/jjC4nHdm/match.jpg" class="img-fluid" style="width: 100%;max-height:90vh"
               alt="...">
             <div class="carousel-caption d-none d-md-block">
               <h5>Third slide label</h5>
@@ -40,7 +40,11 @@
         <div class="container-shopping">
             <h1 class="about-text">About Us</h1>
           </div>
-        <p class="content">Nightcrawler is a gadget selling company that aims to combat power outages in South Africa. Our mission is to provide affordable, efficient, and sustainable solutions to the growing problem of power outages in the country. We have carefully designed our products to cater to the needs of our customers, while also being environmentally conscious.</p>
+        <p class="content">Nightcrawler is a gadget selling company that aims to combat power outages in South Africa. Our mission is to provide affordable, efficient, and sustainable solutions to the growing problem of power outages in the country. We have carefully designed our products to cater to the needs of our customers, while also being environmentally conscious. Nightcrawler is committed to providing sustainable and affordable solutions to 
+          power outages in South Africa. Our products are designed with the needs of our 
+          customers in mind, and we are confident that we will be able to meet the growing 
+          demand for our products. We look forward to serving our customers and 
+          contributing to the development of a sustainable future for South Africa.</p>
         <div class="container text-center">
           <div class="row align-items-center">
             <div class="col-sm-4">
@@ -65,12 +69,24 @@
     <section id="sample">
       <div class="product-c bg-dark-subtle">
         <h1 class="product-text">Featured Products</h1>
-        <div class="card" style="width: 18rem;">
-          <img src="https://i.postimg.cc/QNTpBbhR/battery-charger.png" class="card-img-top" alt="...">
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+        <p class="sub-product">These are some of our big selling products on the market</p>
+        <div class="container text-center">
+          <div class="row align-items-center">
+            <div class="col-sm-4">
+              <div class="image-box">
+                <img class="img-fluid" src="https://i.postimg.cc/QNTpBbhR/battery-charger.png" >
+              </div>
+            </div>
+            <div class="col-sm-4">
+              <div class="image-box">
+                <img class="img-fluid" src="https://i.postimg.cc/g01xdCXr/P-generator.png" >
+              </div>
+            </div>
+            <div class="col-sm-4">
+              <div class="image-box">
+                <img class="img-fluid" src="https://i.postimg.cc/7ZN5HgRb/UPS.png" >
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -102,17 +118,18 @@ export default {
   width: 100%;
   margin: 0 auto;
 }
-.about-text, .product-text {
+.about-text, .product-text, .sub-product {
   font-family: 'Special Elite', cursive;
-  margin-top: 30px;
+  margin-top: 15px;
   text-align: center;
   color: black;
   text-shadow: 2px 2px 4px rgb(134, 131, 131);
   position: relative;
 }
-.content {
+
+.content, .sub-product {
   font-family: 'Tilt Warp', cursive;
-  margin-top: 30px;
+  margin-top: 15px;
   text-align: center;
   color: black;
   font-size: larger;
@@ -134,10 +151,28 @@ export default {
   content: "";
   position: absolute;
   display: block;
-  left: 25%;
+  left: 35%;
   bottom: -10px;
-  width: 50%;
+  width: 30%;
   height: 2px;
   background: rgb(0, 0, 0);
+}
+.image-box {
+  position: relative;
+  margin: auto;
+  overflow: hidden;
+  width: auto;
+  padding: 30px;
+}
+.image-box img {
+  max-width: 100%;
+  transition: all 0.3s;
+  display: block;
+  width: 100%;
+  height: auto;
+  transform: scale(1);
+}
+.image-box:hover img {
+  transform: scale(1.1);
 }
 </style>
